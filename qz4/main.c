cf8039a55908278be6046168e610afdbeefcadbf
duitzu <songlove0604@gmail.com>
2022-04-19 15:04:47 +0800

    main.c

--- qz4/main.c
+++ qz4/main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int* n, int* m) {
+void swap_func1(int* n, int* m) {
 	int temp = *n;
 	*n = *m;
 	*m = temp;
 }
 
-void swapArray(int sourse[], int dest[], int size) {
+void swapArray(int source[], int dest[], int size) {
 	for(int i = 0; i < size; i++) {
-		int temp = sourse[i];
-		sourse[i] = dest[i];
+		int temp = source[i];
+		source[i] = dest[i];
 		dest[i] = temp;
 	}
 }
 
-void printArray(int array[], int size) {
+void printArray(int arr[], int size) {
 	for(int i = 0; i < size; i++) {
 		if(i == size - 1) {
-			printf("%d]\n", array[i]);
+			printf("%d]\n", arr[i]);
 		} else {
-			printf("%d, ", array[i]);
+			printf("%d, ", arr[i]);
 		}
 	}
 }
@@ -36,22 +36,21 @@ char* copy_string(char* s) {
 	return cp_str;
 }
 
-int main(void) {
+int main() {
 	int n, m;
 	n = 1;
 	m = 2;
-	swap(&n, &m);
+	swap_func1(&n, &m);
 	printf("n = %d, m = %d\n", n, m);
 
 	int size = 10;
-	int sourse[10] = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int source[10] = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int dest[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-	swapArray(sourse, dest,size);
-	printf("after swap array, sourse array = [");
-	printArray(sourse, size);
+	swapArray(source, dest,size);
+	printf("after swap array, source array = [");
+	printArray(source, size);
 	printf("after swap array, dest array = [");
 	printArray(dest, size);
-
 	char str[] = "IU!IU!IU!IU!";
 	char* cp_str = copy_string(str);
 	printf("copy string = %s\n", cp_str);
